98436ecf3732a743a14b42d07fc9f3fc84c439f3
Your Name <your.email@example.com>
2026-01-31 20:56:35 +0200

    First commit

--- auth-server.js
+++ auth-server.js
@@ -3,6 +3,18 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// Enable CORS for all routes
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
